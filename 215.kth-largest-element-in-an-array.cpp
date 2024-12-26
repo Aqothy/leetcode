6b60ea2129dcf8a2f4717b1bf7da087195234159
Aqothy <aq0335@gmail.com>
2024-12-26 16:26:25 -0500

    more leetcode solutions

--- 215.kth-largest-element-in-an-array.cpp
+++ 215.kth-largest-element-in-an-array.cpp
@@ -1,8 +1,26 @@
 // @leet start
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        
+  int findKthLargest(vector<int>& nums, int k) {
+    //kth largest element, it is sorted so you can either sort in descending order or you can just use a min heap and then return the kth element
+    //sort by descending order
+    /*sort(nums.begin(), nums.end(), [](int a, int b){*/
+    /*  return b < a;*/
+    /*});*/
+    /**/
+    /*return nums[k-1];*/
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int n : nums) {
+      pq.push(n);
+      if (pq.size() > k) {
+        pq.pop(); // Maintain heap size of k
+      }
     }
+
+    // The root of the min-heap is the Kth largest element
+    return pq.top();
+  }
 };
 // @leet end
--- 973.k-closest-points-to-origin.cpp
+++ 973.k-closest-points-to-origin.cpp
@@ -1,8 +1,27 @@
 // @leet start
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        
+  vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+    //calculate the distance, keep track in a vector of pair that is int and vectora, sort the vector and return k elements
+    // sorting will work since if the elements are pairs then it sorts by the first element of the pair
+
+    int n = points.size();
+    vector<pair<int, vector<int>>> res(n);
+
+    for (int i = 0; i < n; i++){
+      int x = points[i][0];
+      int y = points[i][1];
+      int squaredDistance = x * x + y * y;
+      res[i] = {squaredDistance, points[i]};
     }
+
+    sort(res.begin(), res.end());
+    vector<vector<int>> ans(k);
+    for(int i = 0; i < k; i++){
+      ans[i] = res[i].second;
+    }
+
+    return ans;
+  }
 };
 // @leet end
